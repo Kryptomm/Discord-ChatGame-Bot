6d74052f7c83b6fcecec29164a931f2a72b5671c
Tom Keller <tom11032002@gmail.com>
2023-12-15 17:55:49 +0100

    TicTacToe playable in console

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -19,32 +19,54 @@ Board is classicly build like this:
         2 is Player_Two
 """
 
-def isMovePossible(board):
+def areMovesLeft(board: np.ndarray) -> bool:
     return np.any(board == 0)
 
-def checkWinner(board):
+def checkWinner(board: np.ndarray) -> bool:
     for i in range(3):
-        #Reihe checken
+        #Check Rows
         if np.all(board[i] == board[i][0]):
             return board[i][0]
-        #Spalte checken
+        #Check Columns
         if np.all(board[:,i] == board[:,i][0]):
             return board[:,i][0]
     
-    #Diagonale checken
+    #Check diagonals
     if np.all(board.diagonal() == board.diagonal()[0]):
         return board.diagonal()[0]
     if np.all(np.flipud(board).diagonal() == np.flipud(board).diagonal()[0]):
         return np.flipud(board).diagonal()[0]
     
+    #No winner
     return 0
 
+def playPiece(board: np.ndarray, piece: int, field: int) -> bool:
+    y = field // 3
+    x = field % 3
+    
+    if board[y][x]: return False
+    
+    board[y][x] = piece
+    return True
 
 if __name__ == "__main__":
     board = np.array([
-                [0,0,1],
                 [0,0,0],
-                [1,0,0]
+                [0,0,0],
+                [0,0,0]
             ])
     
-    print(checkWinner(board))
+    current_player = PLAYER_ONE
+    while areMovesLeft(board) and not checkWinner(board):
+        print(board)
+        
+        target_field = int(input("field: "))
+        while(not playPiece(board, current_player, target_field)):
+            target_field = int(input("NEU field: "))
+            
+        if current_player == PLAYER_ONE: current_player = PLAYER_TWO
+        else: current_player = PLAYER_ONE
+        
+    if not checkWinner(board):
+        print("Unentschieden")
+    else: print(checkWinner(board))
